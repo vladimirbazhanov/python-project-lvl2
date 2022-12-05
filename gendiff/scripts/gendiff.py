46e284ca37d4d639f952643a281b1933c53bcb57
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2022-12-05 15:11:32 +0300

    added FORMAT arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,9 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=argparse.FileType('r'))
+    parser.add_argument('second_file', type=argparse.FileType('r'))
+    parser.add_argument('--format', required=False, metavar='FORMAT', default='plain', help='set format of output')
 
     args = parser.parse_args()
     print(args.accumulate(args.integers))
