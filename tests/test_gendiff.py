82f37183e3962e4c3341ef1220e1c883539386dd
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-03-10 19:04:56 +0300

    started working on yaml support

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,9 +2,16 @@ from gendiff.gendiff import generate_diff
 import pytest
 
 
-def test_generate_diff():
-    result = generate_diff('tests/fixtures/file_001.json', 'tests/fixtures/file_002.json')
-    f = open('tests/fixtures/file_001_file_002.txt', 'r')
+def test_generate_diff_json():
+    result = generate_diff('tests/fixtures/json/file_001.json', 'tests/fixtures/json/file_002.json')
+    f = open('tests/fixtures/json/file_001_file_002.result.txt', 'r')
+    expected_result = f.read()
+    f.close()
+    assert result == expected_result
+
+def test_generate_diff_yaml():
+    result = generate_diff('tests/fixtures/yaml/file_001.yml', 'tests/fixtures/yaml/file_002.yml')
+    f = open('tests/fixtures/yaml/file_001_file_002.result.txt', 'r')
     expected_result = f.read()
     f.close()
     assert result == expected_result
