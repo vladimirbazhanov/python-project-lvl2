907f2ae5571e1aba66d042dc1f930b85e1543070
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-04-15 13:08:55 +0300

    some tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,29 +1,55 @@
-import pdb
-
 from gendiff.gendiff import generate_diff
 
-def test_generate_diff_json():
-    result = generate_diff('tests/fixtures/json/file_001.json', 'tests/fixtures/json/file_002.json')
-    with open('tests/fixtures/json/file_001_file_002.result.txt', 'r') as fixture:
+
+def test_generate_diff_json_to_plain():
+    result = generate_diff(
+        'tests/fixtures/json/file_001.json',
+        'tests/fixtures/json/file_002.json', 'plain'
+    )
+    with open('tests/fixtures/json/file_001_file_002.plain.txt', 'r') as fixture:
+        expected_result = fixture.read()
+        assert result == expected_result
+
+
+def test_generate_diff_json_to_stylish():
+    result = generate_diff(
+        'tests/fixtures/json/file_001.json',
+        'tests/fixtures/json/file_002.json',
+        'stylish'
+    )
+    with open('tests/fixtures/json/file_001_file_002.stylish.txt', 'r') as fixture:
         expected_result = fixture.read()
         assert result == expected_result
 
 
 def test_generate_diff_recursive_json_to_plain():
-    result = generate_diff('tests/fixtures/r_json/r_file_001.json', 'tests/fixtures/r_json/r_file_002.json', 'plain')
+    result = generate_diff(
+        'tests/fixtures/r_json/r_file_001.json',
+        'tests/fixtures/r_json/r_file_002.json',
+        'plain'
+    )
     with open('tests/fixtures/r_json/r_file_001_r_file_002.plain.txt', 'r') as fixture:
         expected_result = fixture.read()
         assert result == expected_result
 
+
 def test_generate_diff_recursive_json_to_stylish():
-    result = generate_diff('tests/fixtures/r_json/r_file_001.json', 'tests/fixtures/r_json/r_file_002.json', 'stylish')
+    result = generate_diff(
+        'tests/fixtures/r_json/r_file_001.json',
+        'tests/fixtures/r_json/r_file_002.json',
+        'stylish'
+    )
     with open('tests/fixtures/r_json/r_file_001_r_file_002.stylish.txt', 'r') as fixture:
         expected_result = fixture.read()
         assert result == expected_result
 
 
-def test_generate_diff_yaml():
-    result = generate_diff('tests/fixtures/yaml/file_001.yml', 'tests/fixtures/yaml/file_002.yml')
-    with open('tests/fixtures/yaml/file_001_file_002.result.txt', 'r') as fixture:
+def test_generate_diff_yaml_to_stylish():
+    result = generate_diff(
+        'tests/fixtures/yaml/file_001.yml',
+        'tests/fixtures/yaml/file_002.yml',
+        'stylish'
+    )
+    with open('tests/fixtures/yaml/file_001_file_002.stylish.txt', 'r') as fixture:
         expected_result = fixture.read()
         assert result == expected_result
