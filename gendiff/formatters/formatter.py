4f34ce432e368b279f2addcd3274502a3025f408
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-04-15 12:31:49 +0300

    linter fix

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -5,7 +5,7 @@ ALLOWED_FORMATS = ['stylish']
 
 
 def format_diff(diff_tree, format):
-    if not format in ALLOWED_FORMATS:
+    if format not in ALLOWED_FORMATS:
         raise Exception('Format not supported: ' + format)
 
     formatter = importlib.import_module('gendiff.formatters.' + format)
