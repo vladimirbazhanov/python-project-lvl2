efa883b3673fcf382e0e2837f60ab716dc9b3a0b
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-04-29 14:29:27 +0300

    tests re-written

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,20 +1,36 @@
 import json
+import os
+
 import pytest
 from deepdiff import DeepDiff
 from gendiff.gendiff import generate_diff
-from conftests import file_set_1, file_set_2
 
 
-@pytest.mark.parametrize("file_1,file_2,format,expected_result", file_set_1)
-def test_generate_diff(file_1, file_2, format, expected_result):
-    result = generate_diff(file_1, file_2, format)
+@pytest.fixture(scope='function')
+def prepared_files(request):
+    file1_path, file2_path, result_file_path, formatter_name = request.param
+    fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+    expected_result = open(os.path.join(fixtures_path, result_file_path)).read()
 
-    assert result == expected_result
+    return os.path.join(fixtures_path, file1_path), os.path.join(fixtures_path, file2_path), expected_result, formatter_name
 
 
-@pytest.mark.parametrize("file_1,file_2,format,expected_result", file_set_2)
-def test_generate_diff_json(file_1, file_2, format, expected_result):
-    result_json = json.loads(generate_diff(file_1, file_2, format))
-    expected_result_json = json.loads(expected_result)
+params_set = [
+    ['file1.json', 'file2.json', 'json-to-json.txt', 'json'],
+    ['file1.json', 'file2.json', 'json-to-plain.txt', 'plain'],
+    ['file1.json', 'file2.json', 'json-to-stylish.txt', 'stylish'],
+    ['file1.yml', 'file2.yml', 'yml-to-json.txt', 'json'],
+    ['file1.yml', 'file2.yml', 'yml-to-plain.txt', 'plain'],
+    ['file1.yml', 'file2.yml', 'yml-to-stylish.txt', 'stylish']
+]
 
-    assert DeepDiff(result_json, expected_result_json, ignore_order=True) == {}
+
+@pytest.mark.parametrize(argnames='prepared_files', argvalues=params_set, indirect=True)
+def test_generate_diff(prepared_files):
+    file1_path, file2_path, expected_result, formatter_name = prepared_files
+
+    result = generate_diff(file1_path, file2_path, formatter_name)
+    if formatter_name == 'json':
+        assert DeepDiff(json.loads(result), json.loads(expected_result), ignore_order=True) == {}
+    else:
+        assert result == expected_result
