d1a2296547322baf41e7b444706bc3b575d034a2
Vladimir Bazhanov <vladimir.a.bazhanov@gmail.com>
2023-05-18 16:10:16 +0300

    fixture moved to its own file

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import os
+
+
+@pytest.fixture(scope='function')
+def prepared_files(request):
+    file1_path, file2_path, result_file_path, formatter_name = request.param
+    fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+    expected_result = open(os.path.join(fixtures_path, result_file_path)).read()
+
+    return os.path.join(fixtures_path, file1_path), os.path.join(fixtures_path, file2_path), expected_result, formatter_name
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,18 +1,8 @@
 import json
-import os
 
 import pytest
 from deepdiff import DeepDiff
 from gendiff.gendiff import generate_diff
-
-
-@pytest.fixture(scope='function')
-def prepared_files(request):
-    file1_path, file2_path, result_file_path, formatter_name = request.param
-    fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
-    expected_result = open(os.path.join(fixtures_path, result_file_path)).read()
-
-    return os.path.join(fixtures_path, file1_path), os.path.join(fixtures_path, file2_path), expected_result, formatter_name
 
 
 params_set = [
